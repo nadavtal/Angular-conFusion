97b5add8637f0d4ded856583270ba9317cbffbe9
nadav almagor <nadavtalalmagor@gmail.com>
2018-06-17 18:59:45 +0300

    basic installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'; 
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout'; 
 import { AppComponent } from './app.component';
+import  'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserAnimationsModule,
+    FlexLayoutModule, 
+    MatToolbarModule 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
