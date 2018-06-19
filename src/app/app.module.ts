b8c565f082994659669225028bf100b823664960
nadav almagor <nadavtalalmagor@gmail.com>
2018-06-19 20:52:37 +0300

    basic service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'; 
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material'; 
+import { MatToolbarModule } from '@angular/material';
+
 import { MatListModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout'; 
 import { AppComponent } from './app.component';
 import  'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishService } from './services/dish.service';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +26,9 @@ import { MenuComponent } from './menu/menu.component';
     MatCardModule, 
     
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
