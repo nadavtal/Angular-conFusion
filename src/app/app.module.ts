dbd0a76c97e3dd1dac0a994bf082990f33397b6f
nadav almagor <nadavtalalmagor@gmail.com>
2018-06-20 13:54:35 +0300

    singel page applcation part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
 import  'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -36,11 +37,12 @@ import { HomeComponent } from './home/home.component';
     MatToolbarModule,
     MatListModule,
     MatCardModule, 
-    AppRoutingModule, 
+    AppRoutingModule
     
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
